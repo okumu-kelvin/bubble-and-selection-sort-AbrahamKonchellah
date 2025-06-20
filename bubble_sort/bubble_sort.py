ab3c50a712c4bb7481bb833ed0ef9329bea853cf
Abraham Konchellah <abrahamkonchellah@gmail.com>
2025-06-20 12:15:44 +0300

    Selection Sort Implementation

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,7 +1,7 @@
 def bubble_sort(arr):
     # TODO: Implement bubble sort
 
-#An outer for loop to iterate through the array n times
+    #An outer for loop to iterate through the array n times
     for n in range(len(arr)-1, 0, -1):
         #Initialize a boolean variable to track if any swaps occur
         swapped=False
@@ -11,7 +11,7 @@ def bubble_sort(arr):
                 #Swap items if they are in the wrong order
                 arr[i], arr[i+1]= arr[i+1], arr[i]
                 swapped=True
-#If no swap occurs, the array is sorted
+        #If no swap occurs, the array is sorted
         if not swapped:
             break
 
--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,20 @@
-def selection_sort(arr):
+def selection_sort(array, size):
     # TODO: Implement selection sort
-    pass
+
+        for POS in range(size):
+            smallest=POS
+
+            for j in range(POS + 1, size):
+                # selecting the smallest element in every iteration
+                if array[j] < array[smallest]:
+                    smallest = j
+            # swapping the elements to sort the array
+            (array[POS], array[smallest]) = (array[smallest], array[POS])
+
+arr = [2, 9, 7, 11, 31, 18, -9]
+print("Unsorted array:\n", arr)
+
+size = len(arr)
+selection_sort(arr, size)
+print('Sorted array:')
+print(arr)
